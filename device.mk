e6958f2697ee9da29ef02234a129b0e19c851684
ArchieMeng <archiemeng@protonmail.com>
2025-06-05 20:37:33 +0800

    Include Lindroid vendor

--- device.mk
+++ device.mk
@@ -87,3 +87,4 @@ PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/rhodep/rhodep-vendor.mk)
+$(call inherit-product, vendor/lindroid/lindroid.mk)
